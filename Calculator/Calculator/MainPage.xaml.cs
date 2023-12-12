38dd7926d68fb135eb4e041413a8ba0c148f0810
Kacper Piaskowy <kacperpiaskowy937@gmail.com>
2023-12-12 19:27:33 +0100

    Fornt End UI

--- Calculator/Calculator/MainPage.xaml.cs
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -6,11 +6,25 @@ namespace CalculatorApp
 
     public partial class MainPage : ContentPage
     {
+        private double width = 0;
+        private double height = 0;
         public MainPage()
         {
             InitializeComponent();
         }
 
-        
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            if (width != this.width || height != this.height)
+            {
+                this.width = width;
+                this.height = height;
+                if (this.width > this.height)
+                {
+                    DisplayAlert("teraz", "teraz", "teraz");
+                }
+            }
+        }
     }
 }
